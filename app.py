ba470716af2268dbd6f6f3367f4dadfc738d585f
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-04-14 11:49:54 +0200

    Read CSV file

--- app.py
+++ app.py
@@ -1,33 +1,18 @@
-from sqlalchemy import create_engine, text
+import csv
 
-engine = create_engine("sqlite:///inventory.db", echo=True)
 
 def hello_world():
-    with engine.connect() as conn:
-        result = conn.execute(text("Select 'hello world'"))
-        print(result.all())
-
-
-def create_table():
-    with engine.connect() as conn:
-        conn.execute(text("CREATE TABLE some_table (x int, y int)"))
-
-
-def write_data():
-    with engine.connect() as conn:
-        conn.execute(
-            text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-            [{"x": 11, "y": 12}, {"x": 13, "y": 14}]
-        )
-        conn.commit()
-
-
-def read_data():
-    with engine.connect() as conn:
-        result = conn.execute(text("Select * from some_table"))
-        print(result.all())
+    with open('inventory.csv', 'r') as csv_file:
+        inventory = list(csv.DictReader(csv_file, delimiter=','))
+        
+        first_product = inventory[0]
+        
+        print("====")
+        print(first_product["product_name"])
+        print(first_product["product_price"])
+        print(first_product["product_quantity"])
+        print(first_product["date_updated"])
+        print("====")
 
 if __name__ == "__main__":
-    # write_data()
-    # create_table()
-    read_data()
+    hello_world()
