464f03530ba362c6b03ee02c9331c019d616a83e
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-04-14 11:39:43 +0200

    Now able to write, read, create table

--- app.py
+++ app.py
@@ -2,6 +2,32 @@ from sqlalchemy import create_engine, text
 
 engine = create_engine("sqlite:///inventory.db", echo=True)
 
-with engine.connect() as conn:
-    result = conn.execute(text("Select 'hello world'"))
-    print(result.all())
+def hello_world():
+    with engine.connect() as conn:
+        result = conn.execute(text("Select 'hello world'"))
+        print(result.all())
+
+
+def create_table():
+    with engine.connect() as conn:
+        conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+
+
+def write_data():
+    with engine.connect() as conn:
+        conn.execute(
+            text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+            [{"x": 11, "y": 12}, {"x": 13, "y": 14}]
+        )
+        conn.commit()
+
+
+def read_data():
+    with engine.connect() as conn:
+        result = conn.execute(text("Select * from some_table"))
+        print(result.all())
+
+if __name__ == "__main__":
+    # write_data()
+    # create_table()
+    read_data()
