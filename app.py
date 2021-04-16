aba57f0894cbbab16ae5e95e67c6f2b8f5eedabf
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-04-16 16:48:15 +0200

    Insert rows into db from csv

--- app.py
+++ app.py
@@ -1,6 +1,45 @@
 import csv
+import datetime
 
-from models import Base, engine
+from models import Base, engine, Product, session
+
+
+def clean_price(price_str):
+    parse_to_float = float(price_str.replace("$", ""))
+
+    return int(parse_to_float * 100)
+
+
+def clean_date(date_str):
+    split_date = date_str.split("/")
+
+    month = int(split_date[0])
+    day = int(split_date[1])
+    year = int(split_date[2])
+
+    return datetime.date(year, month, day)
+
+
+def add_csv():
+    with open("inventory.csv") as csv_file:
+        products_data = list(csv.DictReader(csv_file))
+
+        for product in products_data:
+            name = product["product_name"]
+            price = clean_price(product["product_price"])
+            quantity = int(product["product_quantity"])
+            date = clean_date(product["date_updated"])
+
+            new_product = Product(
+                name=name,
+                price=price,
+                quantity=quantity,
+                date=date
+            )
+            session.add(new_product)
+        
+        session.commit()
+
 
 
 def app():
@@ -10,4 +49,5 @@ def app():
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
     
-    app()
+    add_csv()
+    
--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("sqlite:///inventory.db", echo=True)
-Session = sessionmaker()
+engine = create_engine("sqlite:///inventory.db", echo=False)
+Session = sessionmaker(bind=engine)
 session = Session()
 
 Base = declarative_base()
@@ -17,4 +17,7 @@ class Product(Base):
     name = Column("Name", String)
     price = Column("Price", Integer)
     quantity = Column("Quantity", Integer)
-    date = Column("Date", DateTime)
+    date = Column("Date", Date)
+
+    def __repr__(self):
+        return f"Name: {self.name}, Price: {self.price}, Quantity: {self.quantity}"
