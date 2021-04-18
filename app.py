f874a1df144370d5cdf356111fa98ebedb05afdc
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-04-18 19:56:59 +0200

    Fix issue when inserting product in db:
    - Some products were in double. I inserted them once.

--- app.py
+++ app.py
@@ -24,19 +24,24 @@ def add_csv():
     with open("inventory.csv") as csv_file:
         products_data = list(csv.DictReader(csv_file))
 
+        product_test = products_data[2]["product_name"]
+
         for product in products_data:
             name = product["product_name"]
             price = clean_price(product["product_price"])
             quantity = int(product["product_quantity"])
             date = clean_date(product["date_updated"])
 
-            new_product = Product(
-                name=name,
-                price=price,
-                quantity=quantity,
-                date=date
-            )
-            session.add(new_product)
+            product_in_db = session.query(Product).filter(Product.name == name).one_or_none()
+
+            if product_in_db == None:
+                new_product = Product(
+                    name=name,
+                    price=price,
+                    quantity=quantity,
+                    date=date
+                )
+                session.add(new_product)
         
         session.commit()
 
